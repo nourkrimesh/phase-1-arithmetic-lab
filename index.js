f6310d6640c563c9c09b9f2a498e6942d9ced04d
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-01 17:58:15 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+const num3 = 18;
+const num4 = 7;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(3,20,12,4,8,10,1);
